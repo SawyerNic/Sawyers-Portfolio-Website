a644cecc6d052eab623ae64ab564556566fe603e
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-01-21 18:45:54 -0500

    tutorial progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import 'bulma/css/bulma.css';
-import Header from './header.jsx'
-
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
-  return <>
-  <Header /><div  className="has-background-grey">
-    <h1>Projects yay!</h1>
-    <ul>
-      <li>Project 1</li>
-      <li>Project 2</li>
-      <li>Project 3</li>
-      <li>Project 4</li>
-      <li>Project 5</li>
-      <li>Project 6</li>
-    </ul>
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover
+        bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+    <About />
+    <Experience />
+    <Tech />
+    <Works />
+    <Feedbacks />
+    <div className="relative z-0">
+      <Contact />
+      <StarsCanvas />
+      </div>
     </div>
-  </>
-  ;
+   </BrowserRouter>
+  )
+  
 };
 
 export default App;
