d98152c1d887ebf06d156b356aa4754f41b507d3
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-02-03 15:40:12 -0500

    model update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,13 +20,30 @@ const Hero = () => {
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white`}>
-              I'm a New Media Interactive Development major at <br className="sm:block hidden" />Rochester Institute of Technology, and
-              i'm passionate <br className="sm:block hidden" /> about creating beautiful, functional, and accessible <br className="sm:block hidden" /> websites and applications.
+              I'm a New Media Interactive Development major at <br className="sm:block hidden" />Rochester Institute of Technology.
             </p>
           </div>
-        
-          <ComputersCanvas />
       </div>
+
+
+        
+      <ComputersCanvas />
+          <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+            <a href="#about">
+              <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                <motion.dev 
+                  animate={{
+                    y: [0, 24, 0]
+                  }}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: 'loop'
+                  }}
+                  className="w-3 h-3 rounded-full bg-secondary mb-1"/>
+              </div>
+            </a>
+          </div>
     </section>
   )
 }
