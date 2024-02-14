115c812195d2d0e43e78a3fd765ccd10c221c119
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-02-14 10:40:21 -0500

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,46 @@
+import React from "react";
 import Tilt from 'react-tilt'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          My work
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Projects.
+        </h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("","", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px]
+          max-w-3xl leading-[30px]"
+        >
+          This will be where I describe the projects that I have done.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+        />
+        ))}
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
